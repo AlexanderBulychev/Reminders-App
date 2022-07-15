f843ee6ca59ee2e7943ea26e1dc1df9346cd1401
Alexander Bulychev <asbul@yandex.ru>
2022-07-15 14:14:44 +0300

    Initial Commit

---
+++ Reminders App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminders App
+//
+//  Created by asbul on 15.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
